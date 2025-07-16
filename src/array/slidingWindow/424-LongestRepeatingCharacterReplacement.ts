220db262463e11efd7ff0bf049c5082bf72b04af
Katlyn <katlyn880815@gmail.com>
2025-07-16 17:55:49 +0800

    feat: 424

--- src/array/slidingWindow/424-LongestRepeatingCharacterReplacement.ts
+++ src/array/slidingWindow/424-LongestRepeatingCharacterReplacement.ts
@@ -1,38 +1,25 @@
 export default function characterReplacement(s: string, k: number): number {
-    if (s.length <= 0) return 0;
-    let longestLength = k === 0 ? 1 : 0;
+    const charMap = new Map<string, number>(); // 紀錄次元出現次數
     let left = 0;
-    let replaceCounter = 0;
-    let replaceIndex = 0;
+    let right = 0;
+    let maxLength = 0; // 紀錄最長長度
+    let maxCount = 0; // map 中次數最多的字元出現次數
 
-    for(let right = 1; right < s.length; right++) {
-        let countable = false; 
-        if (s[left] === s[right]) {
-            countable = true;
+    for(right = 0; right < s.length; right++) {
+        const char = s[right];
+        charMap.set(char, (charMap.get(char) || 0) + 1);
+        // 比較當前字母與 maxCount 誰大，更新 maxCount
+        maxCount = Math.max(maxCount, charMap.get(char)!);
+
+        // window 長度 - 出現次數最多的字元 > k 時，移動左指針
+        if (right - left + 1 - maxCount > k) {
+            // 把左指針的那個字母踢出去，所以該字要剪一
+            charMap.set(s[left], charMap.get(s[left])! - 1);
+            // 移動左指針
+            left++;
         }
-
-        if (s[left] !== s[right] && replaceCounter < k) {
-            countable = true;
-            replaceCounter++;
-            console.log('可以替換')
-        }
-
-        if (s[left] !== s[right] && replaceIndex === 0) {
-            replaceIndex = right; 
-        } 
-
-        
-        if (countable) {
-            longestLength = longestLength < (right + 1) - left ? (right + 1) -  left : longestLength;
-            continue;
-        }
-
-        if (s[left] !== s[right] && replaceCounter >= k) {
-            replaceCounter = 0;
-            left = replaceIndex;
-            replaceIndex = 0;
-            console.log("不能替換")
-        }
+        // 更新最長長度
+        maxLength = Math.max(maxLength, right - left + 1);
     }
-    return longestLength;
+    return maxLength;
 };
