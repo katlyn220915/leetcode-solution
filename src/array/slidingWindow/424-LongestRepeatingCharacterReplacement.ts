9a5dc20672d39f2d2324434df1697a15a12b3f21
Katlyn <katlyn880815@gmail.com>
2025-07-18 16:20:34 +0800

    chore: add js doc

--- src/array/slidingWindow/424-LongestRepeatingCharacterReplacement.ts
+++ src/array/slidingWindow/424-LongestRepeatingCharacterReplacement.ts
@@ -1,3 +1,13 @@
+/**
+ * @description 424. Longest Repeating Character Replacement
+ * @param {string} s - input string
+ * @param {number} k - number of replacements
+ * @returns {number} - longest length of repeating character replacement
+ * 
+ * 先把題目理解為，找到最長的 substring，其中最多可以替換 k 次字母，把它變成全部都相同字母的情況
+ * 所以只要關注視窗內，出現最多次數的字母，其他不是該字母的字有幾個？是否大於 k？
+ * 如果大於 k 的話，就縮小視窗
+ */
 export default function characterReplacement(s: string, k: number): number {
     const charMap = new Map<string, number>(); // 紀錄次元出現次數
     let left = 0;
